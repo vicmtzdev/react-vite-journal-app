7acf7b625f54ca0318cf2bd197c608e9209b0758
Victor Martinez <vicmtzdev@gmail.com>
2023-07-31 23:09:15 -0600

    navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,26 @@
+import { Box } from '@mui/system';
+import { NavBar } from '../components/NavBar';
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+    return (
+        <>
+
+            <Box sx={{ display: 'flex' }}>
+
+                <NavBar drawerWidth={drawerWidth} />
+                {/* Didebar */}
+
+                <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+
+                    {/* Toolbar */}
+                    {children}
+
+                </Box>
+
+            </Box>
+
+        </>
+    )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,20 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
     <>
-      <Typography variant='h1'>Journal Page</Typography>
+
+      <JournalLayout>
+
+        <Typography>Ipsum ipsum nisi amet eiusmod consectetur consectetur commodo. Amet exercitation et pariatur do nostrud. Quis minim fugiat anim tempor officia eiusmod proident excepteur esse commodo id aliqua. Do officia officia elit ea voluptate voluptate deserunt aute.</Typography>
+
+        {/* Nothing selected */}
+        {/* Noteview */}
+
+      </JournalLayout>
+
+
     </>
   )
 }
