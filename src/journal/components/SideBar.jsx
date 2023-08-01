a35d92077abfd4a17ac1d3849b76461694ff7258
Victor Martinez <vicmtzdev@gmail.com>
2023-07-31 23:49:37 -0600

    sidebar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,52 @@
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
+import { CalendarToday, TurnedInNot } from '@mui/icons-material';
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+    return (
+        <>
+
+            <Box
+                component='nav'
+                sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+            >
+
+                <Drawer
+                    variant='permanent' //temporary
+                    open
+                    sx={{
+                        display: { xs: 'block' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                    }}
+                >
+
+                    <Toolbar>
+                        <Typography variant='h6' noWrap component='div' >Victor Martinez</Typography>
+                    </Toolbar>
+
+                    <Divider />
+
+                    <List>
+                        {
+                            ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'].map(text => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            <CalendarToday />
+                                        </ListItemIcon>
+                                        <Grid container>
+                                            <ListItemText primary={text} />
+                                            <ListItemText secondary={'Nulla aliquip officia et laboris veniam elitix valormorguris'} />
+                                        </Grid>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+
+                </Drawer>
+
+            </Box>
+
+        </>
+    )
+}
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,7 +1,8 @@
+import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
-import { NavBar } from '../components/NavBar';
+import { NavBar, SideBar } from '../components';
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
     return (
@@ -10,11 +11,11 @@ export const JournalLayout = ({ children }) => {
             <Box sx={{ display: 'flex' }}>
 
                 <NavBar drawerWidth={drawerWidth} />
-                {/* Didebar */}
+                <SideBar drawerWidth={drawerWidth} />
 
                 <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
 
-                    {/* Toolbar */}
+                    <Toolbar />
                     {children}
 
                 </Box>
