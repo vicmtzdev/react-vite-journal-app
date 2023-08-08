c0fba438c356fbaae1be4953239cbaba955fdb24
Victor Martinez <vicmtzdev@gmail.com>
2023-08-07 21:04:31 -0600

    singin-google

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
@@ -12,12 +13,16 @@ import { checkingAuthentication, startGoogleSingIn } from '../../store/auth';
 
 export const LoginPage = () => {
 
+  const { status } = useSelector(state => state.auth);
+
   const dispatch = useDispatch();
 
   const { email, password, onInputChange } = useForm({
     email: 'vicmtz@gmail.com',
     password: '123456',
   });
+
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -51,13 +56,13 @@ export const LoginPage = () => {
               <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
 
                 <Grid item xs={12} sm={6}>
-                <Button type='submit' variant="contained" fullWidth> 
+                <Button disabled={isAuthenticating} type='submit' variant="contained" fullWidth> 
                     Login
                   </Button>
                 </Grid>
 
                 <Grid item xs={12} sm={6}>
-                <Button variant="contained" fullWidth onClick={onGoogleSignIn} > 
+                <Button variant="contained" fullWidth onClick={onGoogleSignIn} disabled={isAuthenticating} > 
                     <Google />
                     {/* <Typography sx={{ml: 1}} >Google</Typography> */}
                   </Button>
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,5 @@
-import { checkingCredentials } from './';
+import { singInWithGoogle } from '../../firebase/providers';
+import { checkingCredentials, logout, login } from './';
 
 export const checkingAuthentication = (email, password) => {
     return async (dispatch) => {
@@ -12,6 +13,12 @@ export const startGoogleSingIn = () => {
     return async (dispatch) => {
 
         dispatch(checkingCredentials());
+
+        const result = await singInWithGoogle();
+        if (!result.ok) return dispatch(logout(result.errorMessage));
+
+        dispatch(login(result));
+
 
     }
 }
