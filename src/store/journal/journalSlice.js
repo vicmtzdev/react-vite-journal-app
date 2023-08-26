70dfe07660fe267001f7561d761f80bb52a81e37
Victor Martinez <vicmtzdev@gmail.com>
2023-08-26 17:54:10 -0600

    journal-slice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: true,
+        messageSaved: '',
+        notes: [],
+        active: null,
+        // active: {
+        //     id: 'ABC123',
+        //     title: '',
+        //     body: '',
+        //     date: 123456,
+        //     imageUrls: [],
+        // }
+    },
+    reducers: {
+        addNewEmptyNote: (state, action) => {
+
+        },
+        setActiveNote: (state, action) => {
+
+        },
+        setNotes: (state, action) => {
+
+        },
+        setSaving: (state, action) => {
+
+        },
+        updateNote: (state, action) => {
+
+        },
+        deleteNoteById: (state, action) => {
+
+        },
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { addNewEmptyNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById } = journalSlice.actions;
